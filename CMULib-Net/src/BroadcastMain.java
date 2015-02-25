728ae7bcc5cd7208c3fead9eb7e252bc799b07a4
Kang Huang <kanghuangnj@gmail.com>
2015-02-24 21:40:18 -0500

    HK [broadcast main]

---
+++ CMULib-Net/src/BroadcastMain.java
@@ -0,0 +1,17 @@
+/**
+ * Created by kanghuang on 2/24/15.
+ */
+public class BroadcastMain {
+    public static void main(String[] args){
+        if (args[0].equals("master")){
+            Broadcast tool = new Broadcast();
+            BroadcastServer server = new BroadcastServer();
+            new Thread(server).start();
+           // BroadcastManager keeper = BroadcastManager.getInstance();
+        }
+        else if (args[0].equals("slave")){
+            BroadcastClient client = new BroadcastClient(8888);
+            new Thread(client).start();
+        }
+    }
+}
