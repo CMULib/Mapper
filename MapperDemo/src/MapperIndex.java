ff43bcdbb8341ee78c1a996c59a678f87730b9f2
Kang Huang <kanghuangnj@gmail.com>
2015-03-10 14:41:46 -0400

    HK add index

--- MapperDemo/src/MapperIndex.java
+++ MapperDemo/src/MapperIndex.java
@@ -1,5 +1,34 @@
+import java.net.InetAddress;
+import java.util.ArrayList;
+
 /**
  * Created by kanghuang on 3/10/15.
  */
 public class MapperIndex {
+    class Node {
+
+        InetAddress address;
+        int port;
+
+    }
+
+    Node parent;
+    ArrayList<Node> children;
+
+    public MapperIndex(){
+        parent = new Node();
+        children = new ArrayList<Node>();
+    }
+
+    public String getChildAddress(int i){
+        Node child = this.children.get(i);
+        return child.address.getHostAddress() + child.port;
+    }
+
+    public String getParentAddress(){
+        return parent.address.getHostAddress() + parent.port;
+    }
+
+    
+
 }
