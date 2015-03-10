b632d6619c280fba7f384799e42720cc04f4a175
amaliujia <amaliujia@163.com>
2015-03-10 14:37:05 -0400

    [SD] global master

--- MapperDemo/src/MapperMain.java
+++ MapperDemo/src/MapperMain.java
@@ -2,4 +2,16 @@
  * Created by kanghuang on 3/10/15.
  */
 public class MapperMain {
+    public static void main(String[] args){
+        if(args[0].equals("gmaster")){
+
+        }else if(args[0].equals("lmaster")){
+
+        }else if(args[0].equals("slave")){
+
+        }else{
+            System.err.println("wrong arguments");
+            System.exit(1);
+        }
+    }
 }
